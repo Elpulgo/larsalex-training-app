71cc12e16554d071dde9dc71a66dca7cfd7b2f0c
Elpulgo <oscar.f.larsson@gmail.com>
2018-10-07 17:12:53 +0200

    Changed navigation items and setting text value when navigating to configuration.

--- Kotlin/app/src/main/java/larsalex/training_app/NavigationActivity.kt
+++ Kotlin/app/src/main/java/larsalex/training_app/NavigationActivity.kt
@@ -9,16 +9,12 @@ class NavigationActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_home -> {
+            R.id.navigation_config -> {
                 message.text = "Hej Alex"
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
                 message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
                 return@OnNavigationItemSelectedListener true
             }
         }
